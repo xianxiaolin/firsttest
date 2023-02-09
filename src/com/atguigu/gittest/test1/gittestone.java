af98908b1df654d5c1dd2aa73280fd96018c7d39
xianxiaolin <809245700@qq.com>
2023-02-09 10:46:03 +0800

    创建git测试

---
+++ src/com/atguigu/gittest/test1/gittestone.java
@@ -0,0 +1,13 @@
+package com.atguigu.gittest.test1;
+
+public class gittestone {
+    public static void main(String[] args) {
+        System.out.println("你可曾听说过？");
+        System.out.println("一个从天而降的掌法！！！");
+        System.out.println("如");
+        System.out.println("来");
+        System.out.println("神");
+        System.out.println("掌");
+        System.out.println("！！！！！！！！");
+    }
+}
